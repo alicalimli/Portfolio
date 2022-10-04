146d8ef2443abf3eae7585a2badc9100ddac85d4
Ali Reza <webdeve1083@gmail.com>
2022-10-04 17:44:24 +0800

    RN you can close the Nav list whenever you open it.

--- src/components/Navbar/Navbar.component.jsx
+++ src/components/Navbar/Navbar.component.jsx
@@ -7,6 +7,7 @@ import './Navbar.style.scss'
 import logo from '../../assets/logo.png'
 import { ReactComponent as About } from '../../assets/file-earmark-person.svg'
 import { ReactComponent as HamMenu } from '../../assets/list.svg'
+import { ReactComponent as XMenu } from '../../assets/x-lg.svg'
 import { useRef, useEffect } from 'react'
 
 const Navbar = () => {
@@ -35,14 +36,21 @@ const Navbar = () => {
 
     const navListShow = () => {
         const navList = document.querySelector('nav ul')
-        navList.classList.add('nav-list-show')
+        const ulAttribute = navList.getAttribute('data-toggle') 
+        // navList.classList.add('nav-list-show')
+        if (ulAttribute == 'false') {
+            navList.setAttribute('data-toggle', 'true')
+        } else {
+            navList.setAttribute('data-toggle', 'false')
+        }
     }
 
     return (
         <nav className='paddingSize' ref={navbar}>
             <div>
                 <img src={logo} alt="" />
-                <ul>
+                <ul data-toggle="false">
+                    <XMenu className='x-menu' onClick={navListShow} />
                     <li><Link to="/" className='nav-link'><About className='nav-svg-link' />about</Link></li>
                     <li><Link to="/story" className='nav-link'>story</Link></li>
                     <li><Link to="#" className='nav-link'>project</Link></li>
