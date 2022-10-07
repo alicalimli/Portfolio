9f84cf26006dead0e446a847fa33cbcd60ea6bea
Ali Reza <webdeve1083@gmail.com>
2022-10-07 08:08:20 +0800

    Totally rewritten the Navbar CSS style sheet

--- src/components/Navbar/Navbar.component.jsx
+++ src/components/Navbar/Navbar.component.jsx
@@ -26,18 +26,16 @@ const Navbar = () => {
 
     return (
         <nav>
-            <div>
-                <img src={logo} alt="" />
-                <ul data-toggle="false">
-                    <XMenu className='x-menu' onClick={navListShow} />
-                    <li><a href="#about" className='nav-link'><About className='nav-svg-link' /> about</a></li>
-                    <li><a href="#project" className='nav-link'><Project className='nav-svg-link' /> project</a></li>
-                    <li><a href="#story" className='nav-link'><Story className='nav-svg-link' /> story</a></li>
-                    <li><Link to="#" className='nav-link'><Contact className='nav-svg-link' /> contact</Link></li>
-                    <small>Designed By <i>Ali Reza</i></small>
-                </ul>
-                <HamMenu className='ham-menu' onClick={navListShow} />
-            </div>
+            <img src={logo} alt="" />
+            <ul data-toggle="false">
+                <XMenu className='x-menu' onClick={navListShow} />
+                <li><a href="#about" className='nav-link'><About className='nav-svg-link' /> about</a></li>
+                <li><a href="#project" className='nav-link'><Project className='nav-svg-link' /> project</a></li>
+                <li><a href="#story" className='nav-link'><Story className='nav-svg-link' /> story</a></li>
+                <li><Link to="#" className='nav-link'><Contact className='nav-svg-link' /> contact</Link></li>
+                <small>Designed By <i>Ali Reza</i></small>
+            </ul>
+            <HamMenu className='ham-menu' onClick={navListShow} />
         </nav>
     )
 }
