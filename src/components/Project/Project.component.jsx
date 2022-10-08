8d2b9f7674211f40d8badc45671e1e452adf0387
Ali Reza <webdeve1083@gmail.com>
2022-10-09 05:23:07 +0800

    Finished with the issue that was abaout to make a root for colors

--- src/components/Project/Project.component.jsx
+++ src/components/Project/Project.component.jsx
@@ -1,9 +1,28 @@
 import './Project.style.scss'
 
+import Project1 from '../Project1/Project1.component'
+
+import { gsap } from 'gsap'
+import { useEffect, useRef } from 'react'
+
+
 const Project = () => {
+
+    const project = useRef(null);
+
+    useEffect(() => {
+        gsap.to(project.current, {
+            opacity: 1,
+            delay: 1,
+            duration: 1
+        })
+    }, [])
+
     return (
-        <div id='project'>
+        <div id='project' ref={project}>
             <h1>Project</h1>
+            <Project1 />
+            <Project1 />
         </div>
     )
 }
---
+++ src/components/Project1/Project1.component.jsx
@@ -0,0 +1,19 @@
+import './Project1.style.scss'
+
+const Project1 = () => {
+
+    return (
+        <div className='project-1'>
+            <div className='information'>
+                <h2>Ultra Movies</h2>
+                <p>This Website is all about Downloading Movie, but it's fake, I just made it for practising. The tools that I used to make this Full Responsive Website:</p>
+            </div>
+            <div>
+                <img src="https://images.unsplash.com/photo-1541959833400-049d37f98ccd?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1170&q=80" alt="" />
+            </div>
+        </div>
+    )
+}
+
+
+export default Project1
