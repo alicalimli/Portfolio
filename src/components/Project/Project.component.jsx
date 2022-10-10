c4c3e300b08cb4e3a1f64ae1001518d12535066b
Ali Reza <webdeve1083@gmail.com>
2022-10-10 21:06:07 +0800

    Just for checking

--- src/components/Project/Project.component.jsx
+++ src/components/Project/Project.component.jsx
@@ -31,6 +31,7 @@ const Project = () => {
                 <div>
                     <Project1 data={projectData[0]} image={UltraMovies} />
                     <Project1 data={projectData[1]} image={WeatherAPI} />
+                    <Project1 data={projectData[2]} image='https://images.unsplash.com/photo-1469474968028-56623f02e42e?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1174&q=80' />
                 </div>
             </div>
         </div>
