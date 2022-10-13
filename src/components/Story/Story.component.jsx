609fb96479dec865ec7a474dd098ee7b9f33d133
Ali Reza <webdeve1083@gmail.com>
2022-10-14 07:41:02 +0800

    I'm done with the story section...

--- src/components/Story/Story.component.jsx
+++ src/components/Story/Story.component.jsx
@@ -27,12 +27,13 @@ export default function ControlledAccordions() {
                 <Typography sx={{ width: '33%', flexShrink: 0 }}>
                     Iran
                 </Typography>
-                <img width="30px" src="https://cdn-icons-png.flaticon.com/512/330/330549.png" alt="" />
+                <img src="https://cdn-icons-png.flaticon.com/512/330/330549.png" alt="" />
                 </AccordionSummary>
                 <AccordionDetails>
                 <Typography>
-                    Nulla facilisi. Phasellus sollicitudin nulla et quam mattis feugiat.
-                    Aliquam eget maximus est, id dignissim quam.
+                    I was born in Iran, and was there for nearly 9 years. <br />
+                    Because I was from Afghanistan, I couldn't have a normal life like Others. <br />
+                    I couldn't go to school for some Grades, So my father decided to leave the country and migrate to another.
                 </Typography>
                 </AccordionDetails>
             </Accordion>
@@ -45,14 +46,51 @@ export default function ControlledAccordions() {
                 <Typography sx={{ width: '33%', flexShrink: 0 }}>
                     Afghanistan
                 </Typography>
-                <img width='30px' src="https://cdn-icons-png.flaticon.com/512/330/330574.png" alt="" />
+                <img src="https://cdn-icons-png.flaticon.com/512/330/330574.png" alt="" />
                 </AccordionSummary>
                 <AccordionDetails>
                 <Typography>
-                    Nulla facilisi. Phasellus sollicitudin nulla et quam mattis feugiat.
-                    Aliquam eget maximus est, id dignissim quam.
+                    I was in Afghanistan for 1 month, and the situation of Afghanistan was so bad and wasn't a good place of staying.
+                    After Afghanistan, we migrate to...
                 </Typography>
                 </AccordionDetails>
+            </Accordion>
+            <Accordion className='dropdown' expanded={expanded === 'panel3'} onChange={handleChange('panel3')}>
+                <AccordionSummary
+                expandIcon={<ExpandMoreIcon />}
+                aria-controls="panel1bh-content"
+                id="panel3bh-header"
+                >
+                <Typography sx={{ width: '33%', flexShrink: 0 }}>
+                    India
+                </Typography>
+                <img src="https://cdn-icons-png.flaticon.com/512/206/206606.png" alt="" />
+                </AccordionSummary>
+                <AccordionDetails>
+                <Typography>
+                    I stayed in India for 1 month. <br />
+                    After India, we came to...
+                </Typography>
+                </AccordionDetails>
+            </Accordion>
+            <Accordion className='dropdown' expanded={expanded === 'panel4'} onChange={handleChange('panel4')}>
+                <AccordionSummary
+                expandIcon={<ExpandMoreIcon />}
+                aria-controls="panel1bh-content"
+                id="panel4bh-header"
+                >
+                    <Typography sx={{ width: '33%', flexShrink: 0 }}>
+                        Indonesian
+                    </Typography>
+                    <img src="https://cdn-icons-png.flaticon.com/512/2151/2151329.png" alt="" />
+                    </AccordionSummary>
+                    <AccordionDetails>
+                    <Typography>
+                        Before coming to Indonesia, we were in Singapore and waited for one day in airport and then came to Indonesia. <br />
+                        In one of the city of Indonesia by the name of Jakarta, we've registered ourself, and after that we are called Refugee. <br />
+                        It's been 8/9 years that I'm living in Indonesia with less process from the Community (UNHCR).
+                    </Typography>
+                    </AccordionDetails>
             </Accordion>
         </div>
         </div>
